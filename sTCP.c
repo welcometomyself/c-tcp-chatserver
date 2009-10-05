8d4252287abfeeded9a4ebc058399732c95f68de
sinistral.dextral <sinistral.dextral@57a2583e-3d6b-11de-b438-597f59cd7555>
2009-10-05 10:12:29 +0000

    Final rev

--- sTCP.c
+++ sTCP.c
@@ -28,7 +28,8 @@ void process(int);
 pthread_t th;
 int currentconnections = 0;
 int running = 1;
-char** connectedClientsIP, connectedClientsName;
+char** connectedClientsIP;
+char** connectedClientsName;
 
 static void *threading_socket(void *arg) {
   pthread_detach(pthread_self());
@@ -42,69 +43,108 @@ static void *threading_socket(void *arg) {
 }
 
 void process(int connfd) {
-  ssize_t j, n;
+  ssize_t j;
   char buffer[MAXLEN_MESSAGE] = {0};
-  char * out = "";
+  char clientsList[1024] = {0};
+  char * p_clientsList = (char *) & clientsList;
+  char * clr;
+  int infocut;
+  struct sockaddr_in in_addr;
+  socklen_t in_addr_len = sizeof (struct sockaddr_in);
+  FILE * in = fdopen (connfd, "r+");
+  int i;
+  int clientCount;
 
   while (running == 1) {
       j = 0;
-      while ((n = read(connfd, &buffer[j], 1)) > 0) {
-        if (buffer[j] == '\n') {
-          buffer[j] = 0;
-          break;
-        }
-        j++;
+      
+      if (!fgets(buffer, 26, in)) {
+        fprintf(stdout, "EOF on connection #%d\n", connfd);
+        connectedClientsIP[connfd] = (char*) malloc (15*sizeof (char));
+        connectedClientsName[connfd] = (char*) malloc (15*sizeof (char));
+        break;
       }
+      //printf("IN: %s", buffer);
+      
+      //clr = strchr(tempbuffer, '\r');
+      //*clr = 0;
 
-      if (n < 0) {
-        printf ("[FAIL]\n");
-      }
-
-      printf ("Connection #%d got \"%s\"\n", connfd, buffer);
+      printf ("IN(%d):%s", connfd, buffer);
 
       if (strncasecmp(buffer, "BYE", 3) == 0) {
-        out = "BYE";
-        write(connfd, out, sizeof(out));
+        if (strlen(connectedClientsName[connfd]) > 0) {
+            connectedClientsIP[connfd] = (char*) malloc (15*sizeof (char));
+            connectedClientsName[connfd] = (char*) malloc (15*sizeof (char));
+            fputs("BYE\n", in);
+            fprintf(stdout, "OUT(%d):BYE\n", connfd);
+        } else {
+            fputs("ERROR Not registered\n", in);
+            fprintf(stdout, "OUT(%d):ERROR Not registered\n", connfd);
+        }
         break;
       } else if (strncasecmp(buffer, "NEW", 3) == 0) {
-        write(connfd, "OK", sizeof("OK"));
+        if (strlen(connectedClientsName[connfd]) == 0) {
+            getpeername (connfd, (struct sockaddr *) &in_addr, (socklen_t *)&in_addr_len);
+            fputs("New Client: ", stdout);
+            connectedClientsIP[connfd] = inet_ntoa(in_addr.sin_addr);
+            clr = (char *) 0;
+
+            // Workaround for telnet -.-
+            clr = strchr(buffer, '\r');
+            infocut = 4;
+
+            if (clr == (char *)0) {
+                clr = strchr(buffer, '\0');
+                infocut = 5;
+            }
+
+            strncpy(connectedClientsName[connfd], buffer+4, (int)(clr-(char *)&buffer)-infocut);
+
+            printf("%s (%s)\n", connectedClientsName[connfd], connectedClientsIP[connfd]);
+
+            fputs("OK\n", in);
+            fprintf(stdout, "OUT(%d):OK\n", connfd);
+        } else {
+            fprintf(in, "ERROR Already connected as %s\n", connectedClientsName[connfd]);
+            fprintf(stdout, "OUT(%d):ERROR Already connected as %s\n", connfd, connectedClientsName[connfd]);
+            connectedClientsIP[connfd] = (char*) malloc (15*sizeof (char));
+            connectedClientsName[connfd] = (char*) malloc (15*sizeof (char));
+            break;
+        }
       } else if (strncasecmp(buffer, "INFO", 4) == 0) {
-        
+        if (strlen(connectedClientsName[connfd]) > 0) {
+            clientCount = 0;
+            * clientsList = '\0';
+            for (i=0;i<MAX_CONNECTIONS;i++) {
+              if (strlen(connectedClientsName[i]) > 0) {
+                clientCount++;
+                sprintf(clientsList, "%s %s %s", clientsList, connectedClientsIP[i], connectedClientsName[i]);
+              }
+            }
+            fprintf(in ,"LIST %d%s\n", clientCount, clientsList);
+            fprintf(stdout ,"OUT(%d):LIST %d %s\n", connfd, clientCount, clientsList);
+        } else {
+            fputs("ERROR Not registered\n", in);
+            fprintf(stdout, "OUT(%d):ERROR Not registered\n", connfd);
+            connectedClientsIP[connfd] = (char*) malloc (15*sizeof (char));
+            connectedClientsName[connfd] = (char*) malloc (15*sizeof (char));
+            break;
+        }
+      } else if (strncasecmp(buffer, "\n", 1) != 0) {
+        connectedClientsIP[connfd] = (char*) malloc (15*sizeof (char));
+        connectedClientsName[connfd] = (char*) malloc (15*sizeof (char));
+        fputs("ERROR Unknown command\n", in);
+        fprintf(stdout, "OUT(%d):ERROR Unknown command\n", connfd);
+        break;
       }
 
-      /*
-            regex_t test;
-            test.re_magic = 0;
-            test.re_nsub = 1;
-            test.re_endp = '\0';
-
-            if (puffer == "BYE") {
-              // send BYE to client
-              // remove client and ip from client list
-              printf("%s requested disconnect.\n");
-              break;
-            }
-            else if (regcomp(&test, puffer, 1)) {
-              regexec(&test, puffer, strlen(&puffer), regmatch_t[] _match, 1);
-              // add name and ip to client list
-              // send OK to client
-              printf("%s connected as %s with ip %s.\n");
-              continue;
-            }
-            else if (//INFO) {
-              // if client is in clients list
-                  // send LIST COUNT <IP NAME>,... to client
-              printf("Sent list of clients to %s.\n");
-              continue;
-            }
-            else {
-              // send ERROR WRONG_FORMAT to client
-              // remove client from clients list if present
-              printf("Error in clients %s message, closing connection ...\n");
-              break;
-            }
-       */
+/*
+      for (i=0;i<MAXLEN_MESSAGE;i++) {
+        buffer[i] = 0;
+      }
+*/
   }
+  fclose(in);
   close(connfd);
   return;
 }
@@ -116,11 +156,11 @@ int main(void) {
   const int y = 1;
   int i;
 
-  connectedClientsIP = (char**) malloc (15*sizeof (char*));
-  connectedClientsName = (char**) malloc (15*sizeof (char*));
-  for (i=0;i<15;i++) {
-    connectedClientsIP = (char*) malloc (15*sizeof (char));
-    connectedClientsName = (char*) malloc (15*sizeof (char));
+  connectedClientsIP = (char**) malloc (MAX_CONNECTIONS*sizeof (char*));
+  connectedClientsName = (char**) malloc (MAX_CONNECTIONS*sizeof (char*));
+  for (i=0;i<MAX_CONNECTIONS;i++) {
+    connectedClientsIP[i] = (char*) malloc (15*sizeof (char));
+    connectedClientsName[i] = (char*) malloc (15*sizeof (char));
   }
 
   printf ("Creating socket...\t");
@@ -166,7 +206,7 @@ int main(void) {
         pthread_create(&th,
             NULL,
             &threading_socket,
-            connfd);
+            (void *) connfd);
     }
   }
   exit(EXIT_SUCCESS);
